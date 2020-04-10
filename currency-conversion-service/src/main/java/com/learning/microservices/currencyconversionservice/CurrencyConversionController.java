e88b5c877fcd99c5f177e81bc3b6e5e33ae18af6
Ashish Nayak <ashish04021989@gmail.com>
2020-04-10 07:02:21 -0700

    Implementing Spring cloud sleuth for distributed tracing

--- currency-conversion-service/src/main/java/com/learning/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/learning/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,8 @@ public class CurrencyConversionController {
 	
 	@Autowired
 	private CurrencyExchangeServiceProxy proxy;
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, 
@@ -41,7 +45,7 @@ public class CurrencyConversionController {
 			@PathVariable BigDecimal quantity) {
 		
 		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
-		
+		logger.info("{}",response);
 		return new CurrencyConversionBean(response.getId(),from,to,quantity,response.getConversionMultiplier(),
 				quantity.multiply(response.getConversionMultiplier()),response.getPort());
 	}
